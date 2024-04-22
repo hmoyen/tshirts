163b3ac22ff647e685dabfef1d2a8b65a716c8cd
hmoyen <helenamoyen@usp.br>
2024-04-22 15:49:58 -0300

    🐛 fix bugs

--- organize.py
+++ organize.py
@@ -1,10 +1,9 @@
 import gspread
 import pandas as pd
+from gspread_dataframe import set_with_dataframe
+gc = gspread.service_account(filename='/home/lena/tshirts/camisetas-421115-29a129e1e422.json')
 
-gc = gspread.service_account(filename='/home/lena/camisetas/camisetas-421115-29a129e1e422.json')
-
-sheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1pOwDcS8K-NmGAzcw7x8UO1V3FJPFbpU5AVHpKdlJzao/edit#gid=441120012').sheet1
-
+spreadsheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1pOwDcS8K-NmGAzcw7x8UO1V3FJPFbpU5AVHpKdlJzao/edit#gid=441120012')
 
 # Definir os cabeçalhos esperados
 expected_headers = ['Carimbo de data/hora', 'Endereço de e-mail', 'Nome completo',
@@ -14,7 +13,30 @@ expected_headers = ['Carimbo de data/hora', 'Endereço de e-mail', 'Nome complet
                     'CAMISETA RAQUETES PRETA', 'CAMISETA FEDERER BRANCA']
 
 # Obter todos os dados
-data = sheet.get_all_records(expected_headers=expected_headers)
+sheet = spreadsheet.sheet1
+data = sheet.get_all_records()
+# Apagar as planilhas existentes
+worksheet_personalizadas = None
+worksheet_total_camisetas = None
+worksheet_pedido_pessoa = None
+
+for worksheet in spreadsheet.worksheets():
+    if worksheet.title == "Camisetas Personalizadas":
+        worksheet_personalizadas = worksheet
+    elif worksheet.title == "Total de Camisetas":
+        worksheet_total_camisetas = worksheet
+    elif worksheet.title == "Pedido por Pessoa":
+        worksheet_pedido_pessoa = worksheet
+
+if worksheet_personalizadas:
+    spreadsheet.del_worksheet(worksheet_personalizadas)
+
+if worksheet_total_camisetas:
+    spreadsheet.del_worksheet(worksheet_total_camisetas)
+
+if worksheet_pedido_pessoa:
+    spreadsheet.del_worksheet(worksheet_pedido_pessoa)
+
 
 # Manipular os dados para criar os CSVs
 camisetas_personalizadas = []
@@ -22,21 +44,24 @@ total_camisetas = {}
 pedido_por_pessoa = {}
 
 for row in data:
+    # Normalizar o nome completo
+    nome_completo = row['Nome completo'].strip().lower()
+    
     # Verifica se há personalização em cada camiseta e adiciona ao CSV de camisetas personalizadas
     personalizacao_azul_branca = row['Se personalizada (1), qual o nome atrás?']
     personalizacao_azul_amarela = row['Se personalizada (2), qual o nome atrás?']
     personalizacao_preta_amarela = row['Se personalizada (3), qual o nome atrás?']
     
     if personalizacao_azul_branca:
-        camisetas_personalizadas.append({'Nome': row['Nome completo'], 'Camiseta': 'CAMISETA MODALIDADE TÊNIS AZUL E BRANCA',
+        camisetas_personalizadas.append({'Nome': nome_completo, 'Camiseta': 'CAMISETA MODALIDADE TÊNIS AZUL E BRANCA',
                                          'Tamanho': row['CAMISETA MODALIDADE TÊNIS AZUL E BRANCA'],
                                          'Personalização': personalizacao_azul_branca})
     if personalizacao_azul_amarela:
-        camisetas_personalizadas.append({'Nome': row['Nome completo'], 'Camiseta': 'CAMISETA MODALIDADE TÊNIS AZUL E AMARELA',
+        camisetas_personalizadas.append({'Nome': nome_completo, 'Camiseta': 'CAMISETA MODALIDADE TÊNIS AZUL E AMARELA',
                                          'Tamanho': row['CAMISETA MODALIDADE TÊNIS AZUL E AMARELA'],
                                          'Personalização': personalizacao_azul_amarela})
     if personalizacao_preta_amarela:
-        camisetas_personalizadas.append({'Nome': row['Nome completo'], 'Camiseta': 'CAMISETA MODALIDADE TÊNIS PRETA E AMARELA',
+        camisetas_personalizadas.append({'Nome': nome_completo, 'Camiseta': 'CAMISETA MODALIDADE TÊNIS PRETA E AMARELA',
                                          'Tamanho': row['CAMISETA MODALIDADE TÊNIS PRETA E AMARELA'],
                                          'Personalização': personalizacao_preta_amarela})
     
@@ -52,43 +77,60 @@ for row in data:
                 total_camisetas[camiseta][tamanho_camiseta] += 1
     
     # Criar pedido por pessoa
-    nome_completo = row['Nome completo']
     if nome_completo not in pedido_por_pessoa:
-        pedido_por_pessoa[nome_completo] = {'Total camisetas': 0, 'Total personalizacoes': 0,
-                                             'Valor camisetas': 0, 'Valor personalizacoes': 0}
+        pedido_por_pessoa[nome_completo] = {'Nome completo': nome_completo, 'Total camisetas': 0, 'Total personalizacoes': 0,
+                                             'Valor camisetas': 0, 'Valor personalizacoes': 0,
+                                             'CAMISETA MODALIDADE TÊNIS AZUL E BRANCA': '',
+                                             'CAMISETA MODALIDADE TÊNIS AZUL E AMARELA': '',
+                                             'CAMISETA MODALIDADE TÊNIS PRETA E AMARELA': '',
+                                             'CAMISETA RAQUETES PRETA': '',
+                                             'CAMISETA FEDERER BRANCA': ''}
     for camiseta, tamanho in row.items():
         if camiseta.startswith('CAMISETA') and tamanho:
             pedido_por_pessoa[nome_completo][camiseta] = tamanho
             pedido_por_pessoa[nome_completo]['Total camisetas'] += 1
-            if 'personalizacao' in camiseta.lower():
-                pedido_por_pessoa[nome_completo]['Total personalizacoes'] += 1
 
-# Criar DataFrame para camisetas personalizadas e total de camisetas
-camisetas_personalizadas_df = pd.DataFrame(camisetas_personalizadas)
-total_camisetas_df = pd.DataFrame(total_camisetas)
+# Calcular o número de personalizações para cada pessoa
+for pessoa in pedido_por_pessoa.values():
+    nome_pessoa = pessoa['Nome completo']
+    num_personalizacoes = sum(1 for camiseta in camisetas_personalizadas if camiseta['Nome'] == nome_pessoa)
+    pessoa['Total personalizacoes'] = num_personalizacoes
 
-# Calcular valor total do pedido por pessoa
+# Calcular o valor total do pedido por pessoa
 valor_camiseta = 40
 valor_personalizacao = 15
 
-# Carregar o CSV das camisetas personalizadas
-contagem_nomes = camisetas_personalizadas_df['Nome'].value_counts()
+for pessoa in pedido_por_pessoa.values():
+    pessoa['Valor camisetas'] = pessoa['Total camisetas'] * valor_camiseta
+    pessoa['Valor personalizacoes'] = pessoa['Total personalizacoes'] * valor_personalizacao
+    pessoa['Valor total'] = pessoa['Valor camisetas'] + pessoa['Valor personalizacoes']
 
-# Calcular o número total de personalizações para cada pessoa
-for nome, contagem in contagem_nomes.items():
-    if nome in pedido_por_pessoa:
-        pedido_por_pessoa[nome]['Total personalizacoes'] = contagem
+# Converter a lista de camisetas personalizadas em DataFrame
+camisetas_personalizadas_df = pd.DataFrame(camisetas_personalizadas)
 
-# Recalcular o valor total do pedido por pessoa
-for nome, pedido in pedido_por_pessoa.items():
-    pedido_por_pessoa[nome]['Valor camisetas'] = pedido['Total camisetas'] * valor_camiseta
-    pedido_por_pessoa[nome]['Valor personalizacoes'] = pedido['Total personalizacoes'] * valor_personalizacao
-    pedido_por_pessoa[nome]['Valor total'] = pedido['Valor camisetas'] + pedido['Valor personalizacoes']
+# Converter o total de camisetas em DataFrame
+total_camisetas_df = pd.DataFrame(total_camisetas)
 
-# Converter pedido_por_pessoa em DataFrame
+# Adicionar coluna 'Tamanho' ao DataFrame 'total_camisetas_df'
+total_camisetas_df['Tamanho'] = total_camisetas_df.index
+
+# Renomear índice para 'Tamanho' no DataFrame 'total_camisetas_df'
+total_camisetas_df = total_camisetas_df.rename_axis('Tamanho', axis='columns')
+
+# Converter o pedido por pessoa em DataFrame
 pedido_por_pessoa_df = pd.DataFrame.from_dict(pedido_por_pessoa, orient='index')
 
-# Salvar em CSV
+# Criar páginas na planilha para cada DataFrame
+worksheet_personalizadas = spreadsheet.add_worksheet(title="Camisetas Personalizadas", rows=1000, cols=20)
+set_with_dataframe(worksheet_personalizadas, camisetas_personalizadas_df)
+
+worksheet_total_camisetas = spreadsheet.add_worksheet(title="Total de Camisetas", rows=1000, cols=20)
+set_with_dataframe(worksheet_total_camisetas, total_camisetas_df)
+
+worksheet_pedido_pessoa = spreadsheet.add_worksheet(title="Pedido por Pessoa", rows=1000, cols=20)
+set_with_dataframe(worksheet_pedido_pessoa, pedido_por_pessoa_df)
+
+# Salvar os DataFrames como arquivos CSV
 camisetas_personalizadas_df.to_csv('camisetas_personalizadas.csv', index=False)
-total_camisetas_df.to_csv('total_camisetas.csv')
-pedido_por_pessoa_df.to_csv('pedido_por_pessoa.csv')
+total_camisetas_df.to_csv('total_camisetas.csv', index=False)
+pedido_por_pessoa_df.to_csv('pedido_por_pessoa.csv', index=False)
